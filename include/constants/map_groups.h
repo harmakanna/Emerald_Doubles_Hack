04bc4184ee3cefafb7ca7730f23f6941fec41544
harmakanna <72272517+harmakanna@users.noreply.github.com>
2022-08-08 19:22:52 -0400

    Added a comma as a bugfix

--- include/constants/map_groups.h
+++ include/constants/map_groups.h
@@ -597,6 +597,8 @@
 // gMapGroup_IndoorRoute124
 #define MAP_ROUTE124_DIVING_TREASURE_HUNTERS_HOUSE (0 | (33 << 8))
 
-#define MAP_GROUPS_COUNT 34
+// gMapGroup_IndoorCratercrest
+
+#define MAP_GROUPS_COUNT 35
 
 #endif // GUARD_CONSTANTS_MAP_GROUPS_H

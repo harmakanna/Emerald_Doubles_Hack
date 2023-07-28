e0b3815146dcbd4f59b39519fec3f9489e4c476e
Anna Mayorskiy <72272517+harmakanna@users.noreply.github.com>
2023-07-28 13:37:08 -0400

    Designed NPCs in Cratercrest Town, added Trainer placeholders for Awakening Wood, adjusted number of trainers in Mt. Meteor
    Added flavor text in Pacifidlog regarding new regis.
    Littleroot connections updated for some reason.

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -47,8 +47,8 @@
 #define FLAG_DEFEATED_REGIGIGAS                       0x23 // Unused Flag
 #define FLAG_HIDE_REGIDRAGO                           0x24 // Unused Flag
 #define FLAG_HIDE_REGIELEKI                           0x25 // Unused Flag
-#define FLAG_FLAG_HIDE_REGIGIGAS                      0x26 // Unused Flag
-#define FLAG_UNUSED_0x027    0x27 // Unused Flag
+#define FLAG_HIDE_REGIGIGAS                           0x26 // Unused Flag
+#define FLAG_HIDE_COURTNEY                            0x27 // Unused Flag
 #define FLAG_UNUSED_0x028    0x28 // Unused Flag
 #define FLAG_UNUSED_0x029    0x29 // Unused Flag
 #define FLAG_UNUSED_0x02A    0x2A // Unused Flag

96d54adba1ef4d77bb8ae82db3f4e05d925faf56
Anna Mayorskiy <72272517+harmakanna@users.noreply.github.com>
2023-07-29 01:19:13 -0400

    Added Eevee Evolution Event Script
    Needs extensive testing, but the project builds. Will be tested during first through.

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -49,8 +49,8 @@
 #define FLAG_HIDE_REGIELEKI                           0x25 // Unused Flag
 #define FLAG_HIDE_REGIGIGAS                           0x26 // Unused Flag
 #define FLAG_HIDE_COURTNEY                            0x27 // Unused Flag
-#define FLAG_UNUSED_0x028    0x28 // Unused Flag
-#define FLAG_UNUSED_0x029    0x29 // Unused Flag
+#define FLAG_SPOKE_TO_COZMOS_KID                      0x28 // Unused Flag
+#define FLAG_COZMOS_KID_EVOLVED_EEVEE                 0x29 // Unused Flag
 #define FLAG_UNUSED_0x02A    0x2A // Unused Flag
 #define FLAG_UNUSED_0x02B    0x2B // Unused Flag
 #define FLAG_UNUSED_0x02C    0x2C // Unused Flag
